82df67d3ce781337d7ceda94cbeb4da853e19eda
Haukur Kristinsson <haukzinn@gmail.com>
2019-10-28 18:05:00 +0100

    Initial commit

---
+++ Network/Network.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "./Network.h"
+
+using namespace std;
+
+Network::Network()
+{
+
+}
+
+Network::~Network()
+{
+    
+}
---
+++ Test/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "./Network.h"
+
+using namespace std;
+
+int main() 
+{
+    Network n = Network();
+    return 0;
+}
